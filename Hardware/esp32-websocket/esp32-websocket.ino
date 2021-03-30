5b4346bf461ff1a6d4043c9e42ff743f380d12b2
pulasthieka <pulasthieka@yahoo.com>
2021-03-30 11:55:11 +0530

    Using JSON in esp32

--- Hardware/esp32-websocket/esp32-websocket.ino
+++ Hardware/esp32-websocket/esp32-websocket.ino
@@ -1,100 +1,63 @@
 #include <Arduino.h>
+#include <ArduinoJson.h> // for JSON documents
+
+#include <SPI.h>
+#include <Wire.h>
+#include <HardwareSerial.h> // for setting up custom serial buffers
 
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <WiFiClientSecure.h>
 #include <WebSocketsClient.h>
 
-#include <HardwareSerial.h>
-HardwareSerial MySerial1(1);
+#include <Adafruit_GFX.h> // Display libraries
+#include <Adafruit_SSD1306.h>
 
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET 4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define DISPLAY_ADDR 0x3C
+
+Adafruit_SSD1306 Display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+HardwareSerial MySerial1(1);
 WiFiMulti WiFiMulti;
 WebSocketsClient webSocket;
 
-bool wsconnected = false;
-unsigned long timeStamps;
 unsigned long currentMillis;
 unsigned long previousMillis = 0;
 const long interval = 100;
 
-// DATA Vars
-String TData = "[";
-String EData = "[";
-String BData = "[";
-String SData = "[";
-String PData = "[";
-String TCData = "[";
-String ECData = "[";
-String BCData = "[";
-String SCData = "[";
-String PCData = "[";
-String P_data = "["; //to store ADC (P data) not a buffer
-long PAT = 0;        //for PAT signal
-
-//time Vars
-String Ttime = "[";
-String Etime = "[";
-String Btime = "[";
-String Stime = "[";
-String Ptime = "[";
-String TCtime = "[";
-String ECtime = "[";
-String BCtime = "[";
-String SCtime = "[";
-String PCtime = "[";
+int capacity = 1024;
+const char json[] = "{\"PData\":[],\"SData\":[],\"BData\":[],\"TData\":[],\"EData\":[],\"PCData\":[],\"SCData\":[],\"BCData\":[],\"TCData\":[],\"ECData\":[]}";
+DynamicJsonDocument root(capacity);
 String payload;
 char rx = 'X'; //to read first byte from serial buffer
 
-const char* ssid = "Dialog 4G 055";
-const char* password = "6051A6D3";
-const char* ip = "192.168.8.128";
+const char* ssid = "MiA2";
+const char* password = "12345678";
+const char* ip = "192.168.43.238";
 const int port = 8011;
+bool wsconnected = false;
 
-//WiFiServer server(80);
-
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET 4 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup()
 {
-
-
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  if (!Display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_ADDR))
   { // Address 0x3C for 128x32
     Serial.println(F("SSD1306 allocation failed"));
   }
-  //Display
-  display.display();
-  display.clearDisplay();
-  Welcome();
-  Index();
-  //Serial pins
-  //  pinMode(23, INPUT);
-  //  pinMode(22, INPUT);
-  //Serial com
+
+  displayWelcomeMessage();
+
   Serial.begin(115200);
   MySerial1.begin(115200, SERIAL_8N1, 2, 4); //https://quadmeup.com/arduino-esp32-and-3-hardware-serial-ports/
   Serial2.begin(115200);
-  //Setting Serial Buffer size
-  MySerial1.setRxBufferSize(1024);
-  Serial2.setRxBufferSize(1024);
 
-  //Display
-  ConnectingWIFI();
-  //Connecting to WiFi
-  Serial.println();
-  Serial.print("Connecting to: ");
-  Serial.println(ssid);
+  MySerial1.setRxBufferSize(1024); //Setting Serial Buffer size for 1
+  Serial2.setRxBufferSize(1024); //Setting Serial Buffer size for 2
 
+  displayConnectingStatus();
   WiFiMulti.addAP(ssid, password);
   while (WiFiMulti.run() != WL_CONNECTED) {
     Serial.print(".");
@@ -103,28 +66,22 @@ void setup()
 
   Serial.println("");
   Serial.println("WiFi connected");
-  //Starting server
-  //    server.begin();
-  Serial.println("Server started at...");
+  Serial.println("ESP32 alive at ");
   Serial.println(WiFi.localIP());
-  // server address, port and URL
-  Serial.print("Connecting to IP:");
-  Serial.println(ip);
+
+  ConnectingNode();
   webSocket.begin(ip, port, "/");
 
-  // event handler
+  // Websockets
   webSocket.onEvent(webSocketEvent);
-
-  // use HTTP Basic Authorization this is optional remove if not needed
-  //  webSocket.setAuthorization("user", "Password");
-
-  // try ever 5000 again if connection has failed
-  webSocket.setReconnectInterval(5000);
+  //  webSocket.setAuthorization("user", "Password"); // use HTTP Basic Authorization this is optional remove if not needed
+  webSocket.setReconnectInterval(5000); // try ever 5000 again if connection has failed
+  
+  deserializeJson(root, json); // configure JSON object
 }
 
 void loop()
 {
-  ConnectingNode();
   webSocket.loop();
   Send_data();
   delay(1);
@@ -134,52 +91,41 @@ void Send_data()
 {
   if (wsconnected)
   {
-    Sending();
-    Serial.println("connected to Nodejs");
-    Serial.println("Sending data");
-
-    while (1)
+    displayConnectedStatus(true);
+    while (wsconnected)
     {
       currentMillis = millis();
       while (currentMillis - previousMillis < interval)
       {
         currentMillis = millis();
         delayMicroseconds(5);
-        readPressure();
-        readBuffer(MySerial1);
-        readPressure(); // include in readBuffer to increase Sampling
-        readBuffer(Serial2);
+        readBuffer(MySerial1, root);
+        readBuffer(Serial2, root);
       }
       previousMillis = currentMillis;
-      removeCommas();
 
-
-      payload = "{\"PData\":" + PData + "], \"SData\":" + SData + "] ,\"Stime\":" + Stime + "], \"BData\" : " + BData + "],\"TData\" : " + TData + "], \"EData\" : " + EData + "],\"PCData\":" + PCData +
-      "], \"SCData\":" + SCData + "] , \"BCData\" : " + BCData + "], \"TCData\" : " + TCData + "], \"ECData\" : " + ECData + "]}";
-//       payload = "{\"PData\":" + PData + "],\"Ptime\":" + Ptime + "], \"SData\":" + SData + "] ,\"Stime\":" + Stime + "], \"BData\" : " + BData + "],\"Btime\":" +
-//                Btime + "], \"TData\" : " + TData + "], \"Ttime\":" + Ttime + "],\"EData\" : " + EData + "],\"Etime\":" + Etime +
-//                "],\"PCData\":" + PCData + "],\"PCtime\":" + PCtime + "], \"SCData\":" + SCData + "] ,\"SCtime\":" + SCtime + "], \"BCData\" : " + BCData + "],\"BCtime\":" +
-//                BCtime + "], \"TCData\" : " + TCData + "], \"TCtime\":" + TCtime + "],\"ECData\" : " + ECData + "],\"ECtime\":" + ECtime + "] }";
-      Serial.println(payload);
+      payload = "";
+      serializeJson(root, Serial);//print to serial port
+      serializeJson(root, payload);
       webSocket.sendTXT(payload);
-      resetData();
+      deserializeJson(root, json);
       delay(10);
     }
   }
+  else {
+    displayConnectedStatus(false);
+  }
 }
-void readPressure()
-{
-  P_data.concat(String(analogRead(34)) + ",");
-  delayMicroseconds(10);
-}
+
 // buffer reading function
 boolean active = false;
-String *combineTo;
+char* combineTo;
 String *combineTime;
 String value = "";
-void readBuffer(HardwareSerial buffReader)
+
+void readBuffer(HardwareSerial buffReader, DynamicJsonDocument &doc)
 {
-    
+
   while (buffReader.available())
   { //Reading buffer
     rx = buffReader.read();
@@ -190,125 +136,62 @@ void readBuffer(HardwareSerial buffReader)
         if (active)
         {
           active = false;
-          *combineTo = *combineTo + String(value.toInt()) + ",";
+          JsonArray current = doc[combineTo];
+          current.add(value.toInt());
           value = "";
-//                    *combineTime = *combineTime + String(millis()) + ",";
         }
         break;
       case 'T':
         active = true;
-        combineTo = &TData;
-        combineTime = &Ttime;
+        combineTo = "TData";
         break;
       case 'E':
         active = true;
-        combineTo = &EData;
-        combineTime = &Ttime;
+        combineTo = "EData";
         break;
       case 'I':
         active = true;
-        combineTo = &SData;
-        combineTime = &Stime;
+        combineTo = "SData";
         break;
-      case 'R':
+      case 'c':
         active = true;
-        combineTo = &BData;
-        combineTime = &Btime;
+        combineTo = "BData";
         break;
       case 'P':
         active = true;
-        combineTo = &PData;
-        combineTime = &Ptime;
+        combineTo = "PData";
         break;
       case 't':
         active = true;
-        combineTo = &TCData;
+        combineTo = "TCData";
         break;
       case 'e':
         active = true;
-        combineTo = &ECData;
+        combineTo = "ECData";
         break;
       case 'i':
         active = true;
-        combineTo = &SCData;
+        combineTo = "SCData";
         break;
       case 'r':
         active = true;
-        combineTo = &BCData;
+        combineTo = "BCData";
         break;
       case 'p':
         active = true;
-        combineTo = &PCData;
+        combineTo = "PCData";
         break;
 
       default:
         if (active && isDigit(rx))
         {
           value = value + rx;
-
         }
     }
   }
-  //print state
-  //  Serial.println();
-  //  Serial.print("BData");
-  //  Serial.println(BData);
-  //  Serial.print("SData");
-  //  Serial.println(SData);
-  //  Serial.print("TData");
-  //  Serial.println(TData);
-  //  Serial.print("EData");
-  //  Serial.println(EData);
-}
-void removeComma(String &memRef)
-{
-  if (memRef[memRef.length() - 1] == ',')
-  {
-    memRef.remove(memRef.length() - 1); //removing ending coma
-  }
-}
-void removeCommas() {
-  removeComma(P_data);
-  removeComma(TData);
-  removeComma(EData);
-  removeComma(BData);
-  removeComma(SData);
-  removeComma(PData);
-    removeComma(Ttime);
-  removeComma(Etime);
-  removeComma(Btime);
-  removeComma(Stime);
-  removeComma(Ptime);
-
-  removeComma(TCData);
-  removeComma(ECData);
-  removeComma(BCData);
-  removeComma(SCData);
-  removeComma(PCData);
-}
-void resetData()
-{
-  Serial.println("Resetting");
-  P_data = "[";
-  TData = "[";
-  EData = "[";
-  SData = "[";
-  BData = "[";
-  PData = "[";
-    Ttime = "[";
-  Etime = "[";
-  Btime = "[";
-  Stime = "[";
-  Ptime = "[";
- 
-  //  Control arm
-  TCData = "[";
-  ECData = "[";
-  SCData = "[";
-  BCData = "[";
-  PCData = "[";
 
 }
+
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 
   switch (type) {
@@ -324,18 +207,13 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
       break;
     case WStype_TEXT:
       Serial.printf("[WSc] get text: %s\n", payload);
-
       // send message to server
       webSocket.sendTXT("message here");
       break;
-    case WStype_BIN:
-      Serial.printf("[WSc] get binary length: %u\n", length);
-      //      hexdump(payload, length);
-
-      // send data to server
-      // webSocket.sendBIN(payload, length);
-      break;
     case WStype_ERROR:
+      Serial.printf("[WSc] Error \n");
+      break;
+    case WStype_BIN:
     case WStype_FRAGMENT_TEXT_START:
     case WStype_FRAGMENT_BIN_START:
     case WStype_FRAGMENT:
@@ -344,62 +222,70 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
   }
 
 }
+
+
 //Functions for display
 void configureDisplay()
 {
-  display.clearDisplay();
-  display.setCursor(0, 0); // Start at to  p-left corner
-  display.setTextSize(1);  // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
+  Display.clearDisplay();
+  Display.setCursor(0, 0); // Start at to  p-left corner
+  Display.setTextSize(1);  // Draw 2X-scale text
+  Display.setTextColor(SSD1306_WHITE);
 }
-void Index(void)
+
+void displayWelcomeMessage(void)
 {
   configureDisplay();
-  display.println("160648A");
-  display.println("160145E");
-  display.println("160173L");
-  display.println("160199V");
-  display.display();
+  Display.println("WELCOME!");
+  Display.println("Final Year");
+  Display.println("Project");
+  Display.println("160648A");
+  Display.println("160145E");
+  Display.println("160173L");
+  Display.println("160199V");
+  Display.display();
   delay(2000);
 }
 
-void Welcome(void)
+void displayConnectingStatus(void)
 {
+  Serial.println();
+  Serial.println("Connecting to: " + String(ssid));
   configureDisplay();
-  display.println(" WELCOME!");
-  display.println("  Final");
-  display.println("   Year");
-  display.println(" Project");
-  display.display();
-  delay(2000);
-}
-void ConnectingWIFI(void)
-{
-  configureDisplay();
-  display.println("Connecting to WiFi:");
-  display.println(String(ssid));
-  display.display();
+  Display.println("Connecting to WiFi:");
+  Display.println(String(ssid));
+  Display.display();
   //  delay(1000);
 }
+
 void ConnectingNode(void)
 {
+  Serial.println("Connecting to IP: " + String(ip));
   configureDisplay();
-  display.println("Connected to WiFi!");
-  display.println("Connecting to NodeJS");
-  display.println("Target IP:");
-  display.println(String(ip));
-  display.println("Target port:");
-  display.println(String(port));
-  display.display();
+  Display.println("Connected to WiFi!");
+  Display.println("Connecting to NodeJS");
+  Display.println("Target IP:");
+  Display.println(String(ip));
+  Display.println("Target port:");
+  Display.println(String(port));
+  Display.display();
   //  delay(1000);
 }
 
-void Sending(void)
+void displayConnectedStatus(boolean connStatus)
 {
   configureDisplay();
-  display.println("Connected!");
-  display.println("Sending");
-  display.println("data");
-  display.display();
+  Display.println("Connected? " + connStatus);
+  if (connStatus) {
+    Serial.println("connected to Nodejs");
+    Serial.println("Sending data");
+    Display.println("Sending");
+    Display.println("data");
+  }
+  else {
+    Serial.println("Nodejs Disconnected");
+    Display.println("Disconnected");
+  }
+  Display.display();
   //  delay(2000);
 }
