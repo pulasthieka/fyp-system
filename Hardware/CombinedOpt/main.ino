caeda019be6e9f9eee3b286ac11c41c2497b7156
pulasthieka <pulasthieka@yahoo.com>
2021-06-11 20:25:15 +0530

    Better timing

--- Hardware/CombinedOpt/main.ino
+++ Hardware/CombinedOpt/main.ino
@@ -7,19 +7,19 @@ String message = "{";
 #define PWM_PIN A3
 boolean toggle1 = 0;
 
-const char PAT_CHAR = 'p';
-const char RED_CHAR = 'r';
-const char IR_CHAR = 'i';
-const char BODY_TEMP_CHAR = 't';
+const char PAT_CHAR = 'P';
+const char RED_CHAR = 'R';
+const char IR_CHAR = 'I';
+const char BODY_TEMP_CHAR = 'T';
 const char ENV_TEMP_CHAR = 'E';
-const String IMP_REAL = "A";
-const String IMP_IMG = "a";
+const String IMP_REAL = "B";
+const String IMP_IMG = "b";
 
 void setup() {
   Wire.begin();
   pinMode(PWM_PIN, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(2000000);
+  Serial.begin(115200);
   PPGbegin();
   delay(10); //Max powerup time
   while (!Serial);
@@ -28,7 +28,7 @@ void setup() {
   TCCR1A = 0;// set entire TCCR1A register to 0
   TCCR1B = 0;// same for TCCR1B
   TCNT1  = 0;//initialize counter value to 0
-  OCR1A = 19;// = (16*10^6) / (SAMPLING_RATE*1024) - 1 (must be <65536) 
+  OCR1A = 155;// = (16*10^6) / (SAMPLING_RATE*1024) - 1 (must be <65536)
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
   // Set CS10 and CS12 bits for 1024 prescaler
@@ -36,6 +36,8 @@ void setup() {
   // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
   sei();//allow interrupts
+
+  isAvailablePAT = writeRegister(ADS1115_ADDRESS, ADS1115_REG_POINTER_CONFIG, 0b1100011110000011); //128sps 0b1100011110000011 == ±1.024 V     0b1100010110000011 == = ±2.048 V (default)
 }
 
 void loop() {}
@@ -43,71 +45,87 @@ void loop() {}
 boolean isBodyTempAvailable;
 boolean isEnvTempAvailable;
 int timer_count = 0 ;
-const int IMP_RATIO = 5;
+
+
+
 const int TEMP_RATIO = 10;
+const int IMP_RATIO = TEMP_RATIO / numberOfIncremrnts;
 int event_scaler0 = 0;
 int event_scaler1 = 0;
+int event_scaler3 = 1; // offfset to have the request after command
 int event_scaler4 = 1; // offfset to have the request after command
-int event_scaler5 = 1; // offfset to have the request after command
 
 
 ISR(TIMER1_COMPA_vect) {
   sei();
-  timer_count = (timer_count + 1) % 6; // reset counter for the number of events
+  timer_count = (timer_count + 1) % 5; // reset counter for the number of events
+//  long interruptStart = micros();
+//  Serial.println("interrupted");
+  getPAT();
+  getPPG();
+  //  if (timer_count == 0 ) {
+  //    event_scaler0 = (event_scaler0 + 1) % TEMP_RATIO;
+  //    if (event_scaler0 == 0) {
+  //      isBodyTempAvailable = requestTemp(BodyTempAdd); timeThis(String(timer_count) + ":request body temp:");
+  //      isEnvTempAvailable = requestTemp(EnvTempAdd); timeThis(String(timer_count) + ":request env temp:");
+  //    }
+  //  }
 
-  if (timer_count == 0 ) {
-    event_scaler0 = (event_scaler0 + 1) % TEMP_RATIO;
-    if (event_scaler0 == 0) {
-      isBodyTempAvailable = requestTemp(BodyTempAdd); timeThis(String(timer_count) + ":request body temp:");
-      isEnvTempAvailable = requestTemp(EnvTempAdd); timeThis(String(timer_count) + ":request env temp:");
+  //  if (timer_count == 1 ) {
+  //    event_scaler1 = (event_scaler1 + 1) % IMP_RATIO;
+  //    if (event_scaler0 == 0) {
+  //      requestImp(startFrequency, numberOfIncremrnts, incrementFrequency, settlingCycles); timeThis(String(timer_count) + ":Imp Requested:");
+  //    }
+  //  }
+
+  // if (timer_count == 0 ) {
+  //   getPAT();
+  //   getPPG();
+  // }
+
+  //  if (timer_count == 2 ) {
+  //    getPPG();
+  //  }
+
+  //  if (timer_count == 1 ) {
+  event_scaler1 = (event_scaler1 + 1) % IMP_RATIO;
+  if (event_scaler0 == 0) {
+    requestImp(startFrequency, numberOfIncremrnts, incrementFrequency, settlingCycles); timeThis(String(timer_count) + ":Imp Requested:");
+  }
+  event_scaler3 = (event_scaler3 + 1) % IMP_RATIO;
+  if (event_scaler3 == 0) {
+    if (checkAD5933()) {
+      waitTillImpReady(); //can also use checkImpReady() to check if measurement is ready
+      timeThis(String(timer_count) + ":idle imp 1:");
+      printImp(IMP_REAL, IMP_IMG); //Getting the sample from AD5933 and serial printing
+      timeThis("5:reading imp 1:");
     }
   }
-  
-  if (timer_count == 1 ) {
-    event_scaler1 = (event_scaler1 + 1) % IMP_RATIO;
-    if (event_scaler0 == 0) {
-      requestImp(startFrequency, numberOfIncremrnts, incrementFrequency, settlingCycles); timeThis(String(timer_count) + ":Imp Requested:");
+  //  }
+
+  //  if (timer_count == 2 ) {
+  event_scaler0 = (event_scaler0 + 1) % TEMP_RATIO;
+  if (event_scaler0 == 0) {
+    isBodyTempAvailable = requestTemp(BodyTempAdd); timeThis(String(timer_count) + ":request body temp:");
+    isEnvTempAvailable = requestTemp(EnvTempAdd); timeThis(String(timer_count) + ":request env temp:");
+  }
+  event_scaler4 = (event_scaler4 + 1) % TEMP_RATIO;
+  if (event_scaler4 == 0) {
+    if ( isBodyTempAvailable) {
+      temperatureC = getTemp(BodyTempAdd); timeThis(String(timer_count) + ":received body temp:");
+      SerialPlot(BODY_TEMP_CHAR, 100 * temperatureC);
+      isBodyTempAvailable = false;
     }
-  }
-  
-  if (timer_count == 2 ) {
-    getPAT();
-  }
-  
-  if (timer_count == 3 ) {
-    getPPG();
-  }
-
-  if (timer_count == 4 ) {
-    event_scaler4 = (event_scaler4 + 1) % IMP_RATIO;
-    if (event_scaler4 == 0) {
-      if (checkAD5933()) {
-        waitTillImpReady(); //can also use checkImpReady() to check if measurement is ready
-        timeThis(String(timer_count) + ":idle imp 1:");
-        printImp(IMP_REAL, IMP_IMG); //Getting the sample from AD5933 and serial printing
-        timeThis("5:reading imp 1:");
-      }
+    if ( isEnvTempAvailable) {
+      temperatureC = getTemp(EnvTempAdd); timeThis("15:received body temp:");
+      SerialPlot(ENV_TEMP_CHAR, 100 * temperatureC);
+      isEnvTempAvailable = false;
     }
-  }
-  
-  if (timer_count == 5 ) {
-    event_scaler5 = (event_scaler5 + 1) % TEMP_RATIO;
-    if (event_scaler5 == 0) {
-      if ( isBodyTempAvailable) {
-        temperatureC = getTemp(BodyTempAdd); timeThis(String(timer_count) + ":received body temp:");
-        SerialPlot(BODY_TEMP_CHAR, 100 * temperatureC);
-        isBodyTempAvailable = false;
-      }
-      if ( isEnvTempAvailable) {
-        temperatureC = getTemp(EnvTempAdd); timeThis("15:received body temp:");
-        SerialPlot(ENV_TEMP_CHAR, 100 * temperatureC);
-        isEnvTempAvailable = false;
-      }
-    }
+    //    }
 
   }
-
-
+//  Serial.print("interruptedEnd");
+//  Serial.println(micros() - interruptStart);
 }
 
 
@@ -123,11 +141,12 @@ int getPPG() {
   timeThis(String(timer_count) + "communicate PPG:");
   return 0;
 }
+
 int getPAT() {
   //  GET Pressure
-  isAvailablePAT = writeRegister(ADS1115_ADDRESS, ADS1115_REG_POINTER_CONFIG, 0b1100011110000011); //128sps 0b1100011110000011 == ±1.024 V     0b1100010110000011 == = ±2.048 V (default)
-  timeThis("PAT ADC set:");
-  delayMicroseconds(1000);
+  //  isAvailablePAT = writeRegister(ADS1115_ADDRESS, ADS1115_REG_POINTER_CONFIG, 0b1100011110000011); //128sps 0b1100011110000011 == ±1.024 V     0b1100010110000011 == = ±2.048 V (default)
+  //  timeThis("PAT ADC set:");
+  //  delayMicroseconds(1000);
   if (isAvailablePAT) {
     timeThis("check PAT status:");
     PAT = readRegister(ADS1115_ADDRESS, ADS1015_REG_POINTER_CONVERT); timeThis("21:read PAT reg:");
@@ -138,7 +157,7 @@ int getPAT() {
       digitalWrite(PWM_PIN, LOW);
     }
     timeThis("23:fire PAT motor:");
-    //    writeRegister(ADS1115_ADDRESS, ADS1115_REG_POINTER_CONFIG, 0b1100011110000011); //128sps 0b1100011110000011 == ±1.024 V     0b1100010110000011 == = ±2.048 V (default)
+    writeRegister(ADS1115_ADDRESS, ADS1115_REG_POINTER_CONFIG, 0b1100011110000011); //128sps 0b1100011110000011 == ±1.024 V     0b1100010110000011 == = ±2.048 V (default)
     //    timeThis("2:PAT ADC set:");
   }
 }
