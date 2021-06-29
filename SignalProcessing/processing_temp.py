5db05b3c02e6e7411333a5b007881098d7d0db96
ImalshaG <imalsha.sg@gmail.com>
2021-06-29 21:06:11 +0530

    Detrending

--- SignalProcessing/processing_temp.py
+++ SignalProcessing/processing_temp.py
@@ -1,8 +1,7 @@
-from scipy.signal.signaltools import detrend
 from common import *
 
 def ind_temp(data, flip, time_stamps):
-    # Indices: TMP_rise, TMP_fall, TMP_rise/fall, TMP_baseline, TMP_AUC, TMP_env, TMP_max, TMP_min
+    # Indices: TMP_rise, TMP_fall, TMP_rise/fall, TMP_base, TMP_AUC, TMP_env, TMP_max, TMP_min
     if flip:
         TData = data.TCData
         TCData = data.TData
@@ -19,18 +18,20 @@ def ind_temp(data, flip, time_stamps):
     last_ind = tmp_data.last_valid_index()
     tmp_data = remove_nan(tmp_data[:last_ind])
     plot_raw(tmp_data, np.array(time_points)/T_fs, 'Temperature', T_fs)
+    plt.show()
 
-    tmp_data = detrend(tmp_data)
+    baseline = tmp_data[time_points[0]:time_points[1]]
+    tmp_data = detrend_sig(baseline, tmp_data)
     filtered_temp = uniform_filter1d(tmp_data, size=80)
-
-    baseline = np.mean(tmp_data[time_points[0]:time_points[1]])
+    
+    base = np.mean(baseline)
     max_tmp = max(TData/100)
     min_tmp = min(TData/100)
 
     tmp_ind = long_term_indices(filtered_temp, 60, 120)
     AUC = get_AUC(filtered_temp,time_points[0],len(filtered_temp))
 
-    tmp_ind.append(baseline)
+    tmp_ind.append(base)
     tmp_ind.append(AUC)
     tmp_ind.append(env_temp)
     tmp_ind.append(max_tmp)
@@ -40,3 +41,4 @@ def ind_temp(data, flip, time_stamps):
 data = dataN1
 time_stamps = time_stampsN1
 TMP_indicesN1 = ind_temp(data, True, time_stamps)
+
